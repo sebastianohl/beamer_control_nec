88fc4d760b5983ce0bd4f28a078a98987caab7dd
Sebastian Ohl <sebastian@ohl.de>
2023-05-12 19:20:02 +0200

    now working with sleep mode using binary interface

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Needed Hardware:
 * EPS8266 NodeMCU
+* RS232-C level shifter board (e.g. MAX3232)
 
 Configuration:
 * use idy.py menuconfig to set parameters (e.g. WiFi Password, MQTT Settings...)
@@ -10,8 +11,10 @@ Software:
 * vscode with remote containers extension 
 
 Important:
-* set the correct standby mode or your beamer will not wake up because the RS232 is also on sleep. For P525UL the mode is called "network standby"
+* your NEC P525UL beamer will not go into deep sleep if it is continously asked for its power status. 
+  It will then remain in SLEEP mode regardless of your STANDBY_MODE setting. Therefore, this software will
+  stop asking for the beamer status for 60s to allow deep sleep mode.
 
 NEC Beamer Communication:
-* Binary Format [https://www.sharpnecdisplays.eu/p/download/cp/Products/Projectors/Shared/CommandLists/PDF-ExternalControlManual-english.pdf?fn=ExternalControlManual-english.pdf]
-* ASCII Format (this is used) [https://www.sharp-nec-displays.com/support/webdl/dl_service/data/sl/en/command/common_ascii_e-r1.pdf]
+* Binary Format (this is used) [https://www.sharpnecdisplays.eu/p/download/cp/Products/Projectors/Shared/CommandLists/PDF-ExternalControlManual-english.pdf?fn=ExternalControlManual-english.pdf]
+* ASCII Format (this will not wake up your beamer from deep sleep) [https://www.sharp-nec-displays.com/support/webdl/dl_service/data/sl/en/command/common_ascii_e-r1.pdf]
--- main/beamer.c
+++ main/beamer.c
@@ -22,27 +22,39 @@ uart_handle_t uart = {
     .wait_ticks = 5000 / portTICK_PERIOD_MS,
 };
 
+void calc_crc(char *data, size_t length)
+{
+    char crc = 0;
+    for (size_t i = 0; i < length - 1; ++i)
+    {
+        crc += data[i];
+    }
+    data[length - 1] = crc;
+}
+
 void update_power(struct homie_handle_s *handle, int node, int property)
 {
     if (xSemaphoreTake(beamer_state.mutex, (portTickType)portMAX_DELAY) ==
         pdTRUE)
     {
-        if ((xTaskGetTickCount() - 15000 / portTICK_PERIOD_MS) >
+        // 60 after switch off to let beamer go to deep sleep
+        if ((xTaskGetTickCount() -
+             (beamer_state.state ? 30000 : 60000) / portTICK_PERIOD_MS) >
             beamer_state.last_change)
         {
-            const char cmd[] = "power\r\n\0";
-            uart_write(&uart, cmd, strlen(cmd));
+            char cmd[] = {0x00, 0x85, 0x00, 0x00, 0x01, 0x01, 0x00};
+            calc_crc(cmd, sizeof(cmd));
+            uart_write(&uart, cmd, sizeof(cmd));
             uart_cycle(&uart);
             char value[100] = {0};
             size_t len = 99;
             uart_get_buffer(&uart, value, &len);
-            char status[100] = {0};
-            ESP_LOGI(TAG, "pwr value %d %s", len, value);
-            if (len > 0 && sscanf(value, ">power %s", status) == 1)
+            ESP_LOGI(TAG, "pwr value %d", len);
+            if (len > 0 && value[0] == 0x20 && value[1] == 0x85)
             {
                 beamer_state.state =
-                    (strcmp(status, "on") == 0) ? HOMIE_TRUE : HOMIE_FALSE;
-                ESP_LOGI(TAG, "power status %s", status);
+                    (value[7] == 0x01) ? HOMIE_TRUE : HOMIE_FALSE;
+                ESP_LOGI(TAG, "power status %d", beamer_state.state);
             }
         }
         else
@@ -73,16 +85,19 @@ void write_power(struct homie_handle_s *handle, int node, int property,
         if (strncmp(data, "true", data_len) == 0)
         {
             beamer_state.state = HOMIE_TRUE;
-            const char cmd[] = "power on\r\n\0";
-            ESP_LOGI(TAG, "set pwr got %d %s", strlen(cmd), cmd);
-            uart_write(&uart, cmd, strlen(cmd));
+            // const char cmd[] = "power on\r\n\0";
+            char cmd[] = {0x02, 0x00, 0x00, 0x00, 0x00, 0x00};
+            calc_crc(cmd, sizeof(cmd));
+            ESP_LOGI(TAG, "set pwr on");
+            uart_write(&uart, cmd, sizeof(cmd));
         }
         else
         {
             beamer_state.state = HOMIE_FALSE;
-            const char cmd[] = "power off\r\n\0";
-            ESP_LOGI(TAG, "set pwr got %d %s", strlen(cmd), cmd);
-            uart_write(&uart, cmd, strlen(cmd));
+            char cmd[] = {0x02, 0x01, 0x00, 0x00, 0x00, 0x00};
+            calc_crc(cmd, sizeof(cmd));
+            ESP_LOGI(TAG, "set pwr off");
+            uart_write(&uart, cmd, sizeof(cmd));
         }
         xSemaphoreGive(beamer_state.mutex);
     }
@@ -93,29 +108,66 @@ void update_source(struct homie_handle_s *handle, int node, int property)
     if (xSemaphoreTake(beamer_state.mutex, (portTickType)portMAX_DELAY) ==
         pdTRUE)
     {
-        if ((xTaskGetTickCount() - 15000 / portTICK_PERIOD_MS) >
+        if ((xTaskGetTickCount() - 30000 / portTICK_PERIOD_MS) >
                 beamer_state.last_change &&
             beamer_state.state == HOMIE_TRUE)
         {
-            const char cmd[] = "input\r\n\0";
-            uart_write(&uart, cmd, strlen(cmd));
+            char cmd[] = {0x00, 0x85, 0x00, 0x00, 0x01, 0x02, 0x00};
+            calc_crc(cmd, sizeof(cmd));
+            uart_write(&uart, cmd, sizeof(cmd));
 
             uart_cycle(&uart);
 
             char value[100] = {0};
             size_t len = 99;
             uart_get_buffer(&uart, value, &len);
-            char source[100] = {0};
-            ESP_LOGD(TAG, "source value %d %s", len, value);
-            if (len > 0 && sscanf(value, ">input %s", source) == 1)
+            ESP_LOGI(TAG, "source value %d", len);
+            if (len > 0 && value[0] == 0x20 && value[1] == 0x85)
             {
-                ESP_LOGI(TAG, "source %s", source);
+                ESP_LOGD(TAG, "source 6 0x%x", value[6]);
+                ESP_LOGD(TAG, "source 7 0x%x", value[7]);
+                ESP_LOGD(TAG, "source 8 0x%x", value[8]);
+                char source[100] = {0};
+                switch (value[8])
+                {
+                case 0x01:
+                    strcpy(source, "computer");
+                    break;
+                case 0x21:
+                    switch (value[7])
+                    {
+                    case 0x01:
+                        strcpy(source, "hdmi1");
+                        break;
+                    case 0x02:
+                        strcpy(source, "hdmi2");
+                        break;
+                    };
+                    break;
+                case 0x27:
+                    strcpy(source, "hdbaset");
+                    break;
+                case 0x07:
+                    switch (value[7])
+                    {
+                    case 0x01:
+                        strcpy(source, "usb-a");
+                        break;
+                    case 0x02:
+                        strcpy(source, "lan");
+                        break;
+                    };
+                    break;
+                default:
+                    strcpy(source, "UNKNOWN");
+                    break;
+                };
                 homie_publish_property_value(handle, node, property, source);
             }
         }
         else
         {
-            ESP_LOGD(TAG, "skip source request -> power off");
+            ESP_LOGI(TAG, "skip source request -> power off");
         }
         xSemaphoreGive(beamer_state.mutex);
     }
@@ -129,11 +181,36 @@ void write_source(struct homie_handle_s *handle, int node, int property,
     {
         if (data_len > 0)
         {
+            // 02h 03h 00h 00h 02h 01h <channel> <crc>
             //  hdmi1|hdmi2|computer|hdbaset|usb-a|lan
-            char cmd[100] = {0};
-            snprintf(cmd, 99, "input %.*s\r\n", data_len, data);
-            ESP_LOGI(TAG, "set source got %d %s", strlen(cmd), cmd);
-            uart_write(&uart, cmd, strlen(cmd));
+            char cmd[] = {0x02, 0x03, 0x00, 0x00, 0x02, 0x01, 0x00, 0x00};
+            if (strncmp(data, "hdmi1", data_len) == 0)
+            {
+                cmd[6] = 0xa1;
+            }
+            else if (strncmp(data, "hdmi2", data_len) == 0)
+            {
+                cmd[6] = 0xa2;
+            }
+            else if (strncmp(data, "computer", data_len) == 0)
+            {
+                cmd[6] = 0x01;
+            }
+            else if (strncmp(data, "hdbaset", data_len) == 0)
+            {
+                cmd[6] = 0xbf;
+            }
+            else if (strncmp(data, "usb-a", data_len) == 0)
+            {
+                cmd[6] = 0x1f;
+            }
+            else if (strncmp(data, "lan", data_len) == 0)
+            {
+                cmd[6] = 0x20;
+            }
+            calc_crc(cmd, sizeof(cmd));
+            ESP_LOGI(TAG, "set source got %d", sizeof(cmd));
+            uart_write(&uart, cmd, sizeof(cmd));
         }
         xSemaphoreGive(beamer_state.mutex);
     }
